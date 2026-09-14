a19be77aa43a91a591708f8e266893975a52d03e
DESKTOP-82CN7D2\Administrator <756575622@qq.com>
2026-09-15 01:38:29 +0800

    6.2.0

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION="6.1.0"
+VERSION="6.2.0"
 DOWNLOAD_HOST="https://github.com/mine-Proxy/TCMinerSystem/raw/main/linux"
-ORIGIN_EXEC="tcstminersystem-${VERSION}"
+ORIGIN_EXEC="TCMinerSystem-${VERSION}"
 
 SERVICE_NAME="rustservice"
 
