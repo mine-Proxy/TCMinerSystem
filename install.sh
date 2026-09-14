fb139d2f787227bd7048f7332a34880310b83e48
DESKTOP-82CN7D2\Administrator <756575622@qq.com>
2026-09-15 02:19:26 +0800

    6.2.0

--- install.sh
+++ install.sh
@@ -548,7 +548,7 @@ change_limit() {
 installapp() {
     if [ -n "$1" ]; then
         VERSION="$1"
-        ORIGIN_EXEC="tcstminersystem-${1}"
+        ORIGIN_EXEC="TCMinerSystem-${1}"
     fi
 
     echo $ORIGIN_EXEC
